6b97d8ea10306e0b79e346c8d46920b27ea0e980
Denis Hlebau <lfastik@inbox.ru>
2019-03-24 22:26:48 +0300

    feat: changed class node

--- src/node.js
+++ src/node.js
@@ -1,22 +1,55 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data=data;
+		this.priority=priority;
+		this.parent=null;
+		this.left=null;
+		this.right=null;
 	}
 
 	appendChild(node) {
+
+		if(this.right){
+			return;
+		}
+	
+		if(this.left){	
+				this.right=node;
+		}
+		else{
+			this.left=node;
+			
+		}
 
 	}
 
 	removeChild(node) {
 
+		if(this.left==node){
+		this.left=null;
+		//this.left.parent=null;
+		}
+		else if(this.right==node){
+			this.right=null;
+		//	this.right.parent=null;
+		}
+		else{
+			throw new Error("is not a child");
+
+		}
 	}
 
 	remove() {
-
+		if(this.parent==null){
+			return;
+		}
+	//	this.left.
 	}
 
 	swapWithParent() {
-		
+		if(this.parent==null){
+			return;
+		}
 	}
 }
 
